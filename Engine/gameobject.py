28ba8c515bc3c45fc53ffc5c519661062f25b7d1
bladez88 <vujimmyd@gmail.com>
2025-04-03 17:05:58 -0700

    labeled armor_type in gameobject

--- Engine/gameobject.py
+++ Engine/gameobject.py
@@ -3,5 +3,5 @@ class GameObject:
         self.object_id = object_id
         self.x = x
         self.y = y
-        self.armor_type = armor_type
+        self.armor_type = armor_type # for now, 1 - helmet, 2 - chestplate, 3 - pants, 4 - boots
         self.held_by = None  # None means it's on the ground
